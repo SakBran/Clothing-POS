3feb200f5ea32c62a835745eaf03163e25fa0e4b
Sao Bran Aung <saobran.aung61@gmail.com>
2026-02-07 11:23:45 +0630

    refactor: add comments to clarify purpose of constants in brand constant file

--- app/(main)/brand/constant.ts
+++ app/(main)/brand/constant.ts
@@ -1,8 +1,12 @@
+//ဒါက API ကို ဘယ်လိုခေါ်မလဲဆိုတာကို သတ်မှတ်တာပါ
 export const API_Variable = 'Brand';
+//ဒါက SweetAlert2 မှာသုံးတဲ့ Message ကို သတ်မှတ်တာပါ
 export const Message_Variable = 'Brand';
+// ဒါက React ရဲ့ Router မှာသုံးတဲ့ Link ကို ဘယ်လိုခေါ်မလဲဆိုတာကို သတ်မှတ်တာပါ
 export const Next_Link_Variable = 'brand';
+//ဒါက Table မှာ ဘယ် column တွေကို ပြမလဲဆိုတာကို သတ်မှတ်တာပါ
 export const TableColumns = ['brandName', 'isActive', 'id'];
-
+//ဒါက Form မှာသုံးတဲ့ Data Model ကို သတ်မှတ်တာပါ
 export interface FormData {
     brandName: string;
     isActive: string;
